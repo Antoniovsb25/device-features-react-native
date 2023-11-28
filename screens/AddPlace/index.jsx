9ef4f50638649ac60f1a1c0f8de7b5537d740b0f
Antonio Basilio <antoniovsb25@gmail.com>
2023-11-28 20:09:29 -0300

    feat: storing data in db

--- screens/AddPlace/index.jsx
+++ screens/AddPlace/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import PlaceForm from "../../components/Places/PlaceForm";
+import { insertPlace } from "../../util/database";
 
 function AddPlace({ navigation }) {
-  const createPlaceHandler = (place) => {
+  const createPlaceHandler = async (place) => {
+    await insertPlace(place);
     navigation.navigate("AllPlaces", {
       place: place,
     });
